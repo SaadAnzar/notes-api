8ccea44ab2fdc841f290e419f193e6cfaff789ac
Md Anzar Ahmad <anzarhps@gmail.com>
2025-10-22 01:08:22 +0530

    update: restrict CORS origins to specific domains

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "https://notes-rose.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
